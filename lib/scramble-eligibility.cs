b0993ad2b7e613bf3645a031c16356f1b671e913
Philippe Virouleau <philippe.44@gmail.com>
2024-02-01 14:35:43 +0100

    Init

---
+++ lib/scramble-eligibility.cs
@@ -0,0 +1,36 @@
+Define(
+    "CanScramble333",
+    (PersonalBest(_333) < 20s))
+Define(
+    "CanScramble222",
+    (PersonalBest(_222) < 8s))
+Define(
+    "CanScramble444",
+    (PersonalBest(_444) < 1:00s))
+Define(
+    "CanScramble555",
+    (PersonalBest(_555) < 1:20s))
+Define(
+    "CanScramble666",
+    (PersonalBest(_666) < 3:00s))
+Define(
+    "CanScramble777",
+    (PersonalBest(_777) < 4:30s))
+Define(
+    "CanScrambleSq1",
+    (PersonalBest(_sq1) < 20s))
+Define(
+    "CanScrambleClock",
+    (PersonalBest(_clock) < 10s))
+Define(
+    "CanScrambleClockPermissive",
+    (PersonalBest(_clock) < 12s))
+Define(
+    "CanScramblePyraminx",
+    (PersonalBest(_pyram) < 8s))
+Define(
+    "CanScrambleMegaminx",
+    (PersonalBest(_minx) < 1:30s))
+Define(
+    "CanScrambleSkewb",
+    (PersonalBest(_skewb) < 8s))
---
+++ main.cs
@@ -0,0 +1,27 @@
+ClearWCIF(true)
+
+#include "staff_import.cs"
+
+
+
+#Map(StageLeaders(HasRole("delegate")), WcaId())
+
+Cluster("teams", 5, StaffMembers(), StringProperty("stage-id"),
+  [LimitConstraint("Leaders", HasRole("delegate"), 2, 1)])
+
+#Map(Rounds(), Length(Groups()))
+#Map(Rounds(), Length(Groups()))
+#ExportWCIF("end.json")
+
+#Define("EveryoneAssignmentSet", [AssignmentSet("everyone", true, (GroupNumber() < 20))])
+
+#Define("Scramblers",
+  #Persons(CanScramble777()),
+  #public=true)
+
+# AssignGroups(_333-r1, EveryoneAssignmentSet())
+
+# Map(Events(), AddResults(RoundForEvent(1), Persons(CompetingIn())))
+#AddResults(_222-r1, Persons(CompetingIn(_222)))
+
+ExportWCIF()
